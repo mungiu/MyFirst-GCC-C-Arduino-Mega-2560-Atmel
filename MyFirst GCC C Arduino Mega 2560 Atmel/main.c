9a9588b2a6ac29f95addadb009d382c510831b27
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-11-14 11:07:52 +0100

    Initial commit

---
+++ MyFirst GCC C Arduino Mega 2560 Atmel/main.c
@@ -0,0 +1,18 @@
+/*
+ * MyFirst GCC C Arduino Mega 2560 Atmel.c
+ *
+ * Created: 11/14/2019 10:59:58 AM
+ * Author : andre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
