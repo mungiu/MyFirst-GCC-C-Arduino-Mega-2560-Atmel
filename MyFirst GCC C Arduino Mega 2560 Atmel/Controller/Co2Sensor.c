19287f2f47e9fb4439c60cd655540aa06591f687
peJep <37178384+peJep@users.noreply.github.com>
2019-12-16 12:03:50 +0100

    Doxygen comments for Sensor tasks

--- MyFirst GCC C Arduino Mega 2560 Atmel/Controller/Co2Sensor.c
+++ MyFirst GCC C Arduino Mega 2560 Atmel/Controller/Co2Sensor.c
@@ -28,12 +28,12 @@ mh_z19_return_code_t rc;
 
 void my_co2_call_back(uint16_t ppm);
 
-void getCo2SensorMeasurement2() {
-	puts("Hi from co2");}
-
+/*Doxygen: Reads a measurement from the co2 sensor mh_z19. The driver must be initialized before the method call.
+\Variable ppmReturn: Hold the return value of the co2 measurement*/
 void getCo2SensorMeasurement() {
 	puts("Hi from co2");
 	
+	/*Initializes a variable to hold the value of the co2 measurement*/	
 	ppmReturn=0;
 
 		_delay_ms(50);
@@ -45,19 +45,21 @@ void getCo2SensorMeasurement() {
 		}
 		else
 		{
-			// must give to wait the the callback function was called and data saved
+			// must wait for the callback function to be called called and save data
 			_delay_ms(50);
 			printf("Co2 ppm value returned: %08d\n", ppmReturn);
 		}
 	
 };
 
+/*Doxygen: Callback function to to called from the driver interface. The measurement value is saved to shared abstract data type to be accessed from other parts of the code.
+\variable: data_co2. A pointer to the shared data instance*/
 void my_co2_call_back(uint16_t ppm)
 {
 	//save co2 ppm value
 	ppmReturn=ppm;
 	
-	//save to shared
+	//save to shared abstract data type
 	set_co2_data(data_co2,ppm);
 	set_is_corrupt_data_c(data_co2,false);
 	//data_co2 = create_co2_data(ppm, true);
