b87739921a060d3c300ff202388a54d0b0e76365
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2019-12-18 13:23:50 +0100

    update_test_main.c

--- MyFirst GCC C Arduino Mega 2560 Atmel/Test/test_main.c
+++ MyFirst GCC C Arduino Mega 2560 Atmel/Test/test_main.c
@@ -26,45 +26,48 @@ static char * test_bar() {
 	return 0;
 }
 //test get_co2_value
-char* test_get_co2_data_value() {
-	co2_Data = create_co2_data(32, false);
-	MU_ASSERT("get_co2_data_value:", 32 == get_co2_data(co2_Data));
+static char* test_get_co2_data_value() {
+	int co2_value=150;
+	co2_Data = create_co2_data(co2_value, false);
+	MU_ASSERT("error, co2_value != get_co2_data(co2_Data)", co2_value == get_co2_data(co2_Data));
 	return 0;
 }
 
-char* test_set_co2_value(){
-		co2_Data = create_co2_data(12, false);
-		set_co2_data(co2_Data,10);
-		MU_ASSERT("co2_ value:", 10 == get_co2_data(co2_Data));
+static char* test_set_co2_value(){
+		int co2_value=150;
+		co2_Data = create_co2_data(co2_value, false);
+		co2_value=200;
+		set_co2_data(co2_Data,co2_value);
+		MU_ASSERT("Error: co2_Data.co2_data_value!= 200 ", 10 == get_co2_data(co2_Data));
 
 }
 char* test_null_co2_obj(){
 	co2_Data = NULL;
-	MU_ASSERT("co2 null:", NULL == co2_Data);
+	MU_ASSERT("error: co2_Data != NULL", NULL == co2_Data);
 	return 0;
 }
 
 char* test_create_light_and_get_fullRaw(){
 	light_Data=create_light_data(12,23,12,23,false);
-	MU_ASSERT("light obj", 12==get_fullRaw(light_Data));
+	MU_ASSERT("error:  fullRaw!=23", 12 == get_fullRaw(light_Data));
 	return 0;
 }
 char* test_set_light_infrared(){
 	light_Data=create_light_data(12,23,12,23,false);
 	set_light_data(light_Data,10,10,10,19);
-	MU_ASSERT("light infrared",0!= get_infraredRaw(light_Data));
+	MU_ASSERT("error: light_Data!=12 ",0!= get_infraredRaw(light_Data));
 return 0;
 	
 }
 
 char* test_create_tempHum_and_get_hum(){
 	temp_hum=create_temp_hum_data(35,900,false);
-	MU_ASSERT("temperature:", 900==get_hum_data(temp_hum));
+	MU_ASSERT("error: humidity!=900", 900==get_hum_data(temp_hum));
 	return 0;
 }
 char* test_create_tempHum_and_get_temp(){
 	temp_hum=create_temp_hum_data(29,700,false);
-	MU_ASSERT("temperature:", 29==get_temp_data(temp_hum));
+	MU_ASSERT("error: temperature!=29", 29==get_temp_data(temp_hum));
 	return 0;
 }
 static char * all_tests() {
@@ -84,6 +87,7 @@ static char * all_tests() {
 
 
 int main(int argc, char **argv) {
+	
 	char *result = all_tests();
 	if (result != 0) {
 		printf("%s\n", result);
